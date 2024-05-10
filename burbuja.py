b4e306a5a4cb684cd48236b7d030445093a4a84f
Angela Maldonado Graniel (Angie) <158494978+Magnesio04@users.noreply.github.com>
2024-05-10 17:25:52 -0600

    Add files via upload

---
+++ burbuja.py
@@ -0,0 +1,11 @@
+def burbuja(arr):
+    n = len(arr)
+    for i in range(n - 1):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+arr = [64, 34, 25, 12, 22, 11, 90]
+burbuja(arr)
+print("Lista ordenada usando Burbuja:")
+print(arr)
